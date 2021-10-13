2ba2987274ede8ff8ee55cd17391d6c52261427f
Bhuwan Panta <ricky.pantha@gmail.com>
2021-10-13 09:24:08 +0545

    2 programs made

---
+++ 2.area_cube.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    int l;
+    printf("Enter the length of cube: ");
+    scanf("%d",&l);
+    int area=6*l*l;
+    printf("The surface area of cube is %d\n",area);
+}
