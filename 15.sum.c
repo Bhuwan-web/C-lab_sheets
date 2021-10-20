a9d93b0568caa7c37f8957cbec6bf7b1fcbc67c0
Bhuwan Panta <ricky.pantha@gmail.com>
2021-10-20 11:10:00 +0545

    Updated

--- 15.sum.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    int a,b,c,d,e;
-    printf("Enter 5 numbers: ");
-    scanf("%d%d%d%d%d",&a,&b,&c,&d,&e);
-    int sum=a+b+c+d+e;
-    printf("The sum of 5 input numbers is %d\n",sum);
-}
--- 5.quotient_rem.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-int main(){
-    int a=15,b=10;
-    int q=a/b;
-    int d=a%b;
-    printf("The quotient is %d \nThe remainder is %d",q,d);
-}
