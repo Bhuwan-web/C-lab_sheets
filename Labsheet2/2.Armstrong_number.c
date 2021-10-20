c94babd7fa83059cbe2a18b3bffa35241dccde50
Bhuwan Panta <ricky.pantha@gmail.com>
2021-10-20 19:19:53 +0545

    [Updated] armstrng program updated

--- Labsheet2/2.Armstrong_number.c
+++ Labsheet2/2.Armstrong_number.c
@@ -1,3 +1,5 @@
+//Armstrong number 
+// numbers like 153=1^3+5^3+3^3 is called armstrong number
 #include<stdio.h>
 int main()
 {
@@ -10,8 +12,9 @@ for(int i=1;i<=500;i++){
         int rem=num%10;
         num=num/10;
         sum+=rem*rem*rem;
-    }
-    
+    };
+    if(i==sum){
     printf("%d : %d\n",i,sum);
+    }
 }
 }
